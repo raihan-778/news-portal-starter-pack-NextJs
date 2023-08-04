2a714f816a1edbe5f8c757e2cfad11afde1d186e
raihan-778 <raihanarif778@gmail.com>
2023-08-04 12:20:33 +0600

    redux added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import RootLayout from "@/components/Layouts/RootLayout";
 import Banner from "@/components/UI/Banner";
-import AllNews from "@/components/UI/allNews";
+import AllNews from "@/components/UI/AllNews";
 
 const HomePage = ({ allNews }) => {
   console.log(allNews);
@@ -27,7 +27,7 @@ HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
 };
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   const res = await fetch("http://localhost:5000/news");
   const data = await res.json();
   console.log(data);
@@ -36,6 +36,8 @@ export const getStaticProps = async () => {
     props: {
       allNews: data,
     },
-    revalidate: 10, // here revalidate is used to rebuild this specific page after 10 seconds.So that we can get any update info within few seconds. This is a property of getStaticProps function.
+    // revalidate: 10,//If we use getServerSideProps,we do not need to use revalidate because getServerSiderProps create page at the data rendar time so that we can get all updated data at the time of build
+
+    // here revalidate is used to rebuild this specific page after 10 seconds.So that we can get any update info within few seconds. This is a property of getStaticProps function.
   };
 };
--- src/pages/news/[newsId].js
+++ src/pages/news/[newsId].js
@@ -1,64 +1,73 @@
-import RootLayout from "@/components/Layouts/RootLayout";
-import { Card, Col, Image } from "antd";
-import Meta from "antd/es/card/Meta";
-import React from "react";
-
+import { Col, Image, Row } from "antd";
+import RootLayout from "@/components/Layouts/RootLayout"; //please be carefule to import RootLayout after all other component import.
+import {
+  CalendarOutlined,
+  CommentOutlined,
+  ProfileOutlined,
+} from "@ant-design/icons";
 
 const NewsDetailPage = ({ news }) => {
+  if (!news) {
+    return <h1>Loading......</h1>;
+  }
   return (
     <div>
-      <Col key={news.id} className="gutter-row" span={6}>
-        <Card
-          hoverable
-          cover={
+      <Row
+        gutter={{
+          xs: 8,
+          sm: 16,
+          md: 24,
+          lg: 32,
+        }}
+      >
+        <Col className="gutter-row" span={12}>
+          <div>
             <Image
               src={news?.image_url}
-              // width={500}
-              // height={200}
+              width={500}
+              height={200}
               responsive
               alt="news image"
             />
-          }
-        >
-          <Meta title={news?.title} />
-          <div
-            className="line"
-            style={{
-              height: "5px",
-              margin: "20px 0",
-              background: "#000",
-              width: "100%",
-            }}
-          ></div>
-
-          <p
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              width: "100%",
-              color: "gray",
-              margin: "10px 0px",
-              fontSize: "12px",
-            }}
-          >
-            <span>
-              <CalendarOutlined /> {news?.release_date}
-            </span>
-            <span>
-              <CommentOutlined /> {news?.comment_count} COMMENTS
-            </span>
-            <span>
-              <ProfileOutlined /> {news?.category}
-            </span>
-          </p>
-
-          <p style={{ fontSize: "15px" }}>
-            {news?.description.length > 100
-              ? news?.description.slice(0, 70) + "..."
-              : news?.description}
-          </p>
-        </Card>
-      </Col>
+          </div>
+        </Col>
+        <Col className="gutter-row" span={12}>
+          <div>
+            <h1 style={{ fontSize: "25px" }}>{news?.title}</h1>
+            <h6 style={{ fontSize: "16px" }}>{news?.author}</h6>
+            <div
+              className="line"
+              style={{
+                height: "5px",
+                margin: "20px 0",
+                background: "#000",
+                width: "100%",
+              }}
+            ></div>
+            <p
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "100%",
+                color: "gray",
+                margin: "10px 0px",
+                fontSize: "12px",
+              }}
+            >
+              <span>
+                <CalendarOutlined /> {news?.release_date}
+              </span>
+              <span>
+                <CommentOutlined /> {news?.comment_count} COMMENTS
+              </span>
+              <span>
+                <ProfileOutlined /> {news?.category}
+              </span>
+            </p>
+            <p style={{ fontSize: "15px" }}>{news?.description}</p>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
@@ -69,17 +78,17 @@ NewsDetailPage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
 };
 
-export const getStaticPaths = async () => {
+/* export const getStaticPaths = async () => {
   const res = await fetch("http://localhost:5000/news");
   const newses = await res.json();
   const paths = newses.map((news) => ({
     params: { newsId: news.id }, //here id after params should be the name of dynamic pageId which we have create for dynamin render. here id should be named newsId
   }));
 
-  return { paths, fallback: false };
+  return { paths, fallback: false }; //here if we use fallback:false it will create only the maped ammount of data. that means if we get 4 data after map then we can get only 4 static page & if we hit to 5 th dynamic data it will throw us in error page.But it we use fallback:true.Then whatever page we hit it will create this amount of static blank page and wait for the page waitin in a loading state.But if we use fallback:"blocking" then it will not wait in loading state it directly create the user requested static page even if this dyami path is not exist in mapped data.
 };
-
-export const getStaticProps = async (context) => {
+ */
+export const getServerSideProps = async (context) => {
   const { params } = context;
   const res = await fetch(`http://localhost:5000/news/${params.newsId}`);
   const data = await res.json();
